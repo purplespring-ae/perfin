f87bc3f57d2882bcb61a16343c0afd6057a0b2fb
purplespring-ae <alexander.evans@gmx.com>
2023-05-08 10:47:30 +0100

    Added functionality to move input csv to archive and dynamically save imported transactions to csv

--- perfin.py
+++ perfin.py
@@ -12,18 +12,17 @@ import perflog
 
 # CLASS VARIABLES
 
-# class Account():
-   
-
-
 # GLOBAL VARIABLES
 INPUT_DIR = "./raw/new"
+ARCHIVE_DIR = "./raw/archive"
 CSV_DIR = "./raw/pf" # for combined CSV files
+
+FNAME_DATE = "%Y-%m-%d"
 
 # INIT FUNCTIONS
 
 def grab_input():
-    '''Look in INPUT_DIR for .csv files and store as single csv in CSV_DIR'''
+    '''Look in INPUT_DIR for .csv files, create dataframe, move csv to archive'''
     input_files = (
         [os.path.join(INPUT_DIR, fname) for fname in os.listdir(INPUT_DIR)
          if fname.endswith('.csv')]
@@ -38,16 +37,21 @@ def grab_input():
         df = pd.read_csv(f, index_col=False)
         df.columns = df.columns.str.strip()
         dfs.append(df)
-
         
     merged_df = pd.concat(dfs)
     merged_df = merged_df.reset_index(drop=True) # rectify duplicate indices
-    # merged_df.to_csv("test.csv")
+    merged_df["Date"] = pd.to_datetime(merged_df["Date"], format="mixed") # CC statement 
 
-    earliest_date = merged_df["Date"].min().strftime("%Y-%M-%D")
-    latest_date = merged_df["Date"].max().strftime("%Y-%M-%D")
-    logger.info(earliest_date, latest_date)
-
+    earliest_date = merged_df["Date"].min().strftime(FNAME_DATE) 
+    latest_date = merged_df["Date"].max().strftime(FNAME_DATE)
+    today_date = datetime.today().strftime(FNAME_DATE)
+    logger.info("Transactions found for this date range: %s to %s", earliest_date, latest_date)
+    # save dataframe to csv - TODO: not needed?
+    fname = f"transactions_{earliest_date}_to_{latest_date}_imported_{today_date}"
+    merged_df.to_csv(os.path.join(CSV_DIR,fname))
+    for f in input_files:
+        # move iput csv to archive
+        os.rename(f, os.path.join(ARCHIVE_DIR, os.path.basename(f)))
 
 logger = perflog.logger(terminal_level=logging.DEBUG, file_level=0)
 
