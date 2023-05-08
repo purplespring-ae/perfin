8c394f8f44fe4bb4be729ea0b484d2966edf8132
purplespring-ae <alexander.evans@gmx.com>
2023-05-08 13:28:36 +0100

    Split import into sub-functions for readability

--- perfin.py
+++ perfin.py
@@ -31,7 +31,6 @@ FNAME_DATE = "%Y-%m-%d"
 
 def import_new_csv():
     '''Look in INPUT_DIR for .csv files, create dataframe, move csv to archive'''
-
     input_files = (
         [os.path.join(INPUT_DIR, fname) for fname in os.listdir(INPUT_DIR)
         if fname.endswith('.csv')]
@@ -40,65 +39,87 @@ def import_new_csv():
         logger.warning("No input files found.")
         return
 
-    dfs = []
-    for f in input_files:
-        # read CSV using index_col=False to avoid CC misalignment
-        df = pd.read_csv(f, index_col=False)
-        df.columns = df.columns.str.strip()
-        dfs.append(df)
-        
-    merged_df = pd.concat(dfs)
+    def get_input_files():
+        dfs = []
+        for f in input_files:
+            # read CSV using index_col=False to avoid CC misalignment
+            df = pd.read_csv(f, index_col=False)
+            df.columns = df.columns.str.strip()
+            dfs.append(df)
+            
+        merged_df = pd.concat(dfs)
+        return merged_df
 
-    # Reformat: Standardise account name
-    merged_df["Account Name"] = merged_df["Account Name"].replace(
-        {"HOME": "HOME",
-         "Saver": "SAVER",
-         "'A W EVANS": 'CREDIT'}
-    )
-    # Reformat: type Value
-    merged_df["Value"] = merged_df["Value"].astype(float)
-    # Reformat: add Debit and Credit columns
-    merged_df["Debit"] = ""
-    merged_df["Credit"] = ""
-    # Reformat: Split "Value" into Dr Cr depending on if credit card
-    is_cc = (merged_df["Account Name"] == "CREDIT")
-    is_pos = (merged_df["Value"] > 0)
-    is_neg = (merged_df["Value"] < 0)
+    def standardise_input_data(df):
+        # Standardise account name
+        df["Account Name"] = df["Account Name"].replace(
+            {"HOME": "HOME",
+            "Saver": "SAVER",
+            "'A W EVANS": 'CREDIT'})
+        # Reformat: rectify duplicate indices
+        df = df.reset_index(drop=True) 
+        # Reformat: standardise and type Date
+        df["Date"] = pd.to_datetime(df["Date"], format="mixed")
 
-    merged_df["Credit"] = np.where(is_cc & is_pos, merged_df["Value"],"")
-    merged_df["Debit"] = np.where(is_cc & is_neg, abs(merged_df["Value"]),"")
-    merged_df["Credit"] = np.where(~is_cc & is_pos, merged_df["Value"],"")
-    merged_df["Debit"] = np.where(~is_cc & is_neg, abs(merged_df["Value"]),"")
-
-    merged_df = merged_df.drop(columns=["Value","Account Number"])
+        return df
     
+    def add_columns(df):
+        # add columns for expense/income categorisation
+            df["Type"] = "TBC"
+            df["Category"] = "TBC"
+            df["Sub-Category"] = "TBC"
+
+            return df
+
+    def format_tr_value(df):
+        # Reformat: type Value
+        df["Value"] = df["Value"].astype(float)
+        # Reformat: add Debit and Credit columns
+        df["Debit"] = ""
+        df["Credit"] = ""
+        # Reformat: Split "Value" into Dr Cr depending on if credit card
+        is_cc = (df["Account Name"] == "CREDIT")
+        is_pos = (df["Value"] > 0)
+        is_neg = (df["Value"] < 0)
+        df["Credit"] = np.where(is_cc & is_pos, df["Value"],"")
+        df["Debit"] = np.where(is_cc & is_neg, abs(df["Value"]),"")
+        df["Credit"] = np.where(~is_cc & is_pos, df["Value"],"")
+        df["Debit"] = np.where(~is_cc & is_neg, abs(df["Value"]),"")
+        return df
+
+    def trim_input_bloat(df):
+        # Reformat: trim bloat from Description
+        df["Description"] = df["Description"].str.lstrip("'")
+        # Reformat: drop unneeded columns
+        df = df.drop(columns=["Value","Account Number"])
+        return df    
  
-    # Reformat: rectify duplicate indices
-    merged_df = merged_df.reset_index(drop=True) 
-    # Reformat: standardise and type Date
-    merged_df["Date"] = pd.to_datetime(merged_df["Date"], format="mixed")
-    # Reformat: trim bloat from Description
-    merged_df["Description"] = merged_df["Description"].str.lstrip("'")
-
-    # add columns for expense/income categorisation
-    merged_df["Type"] = "TBC"
-    merged_df["Category"] = "TBC"
-    merged_df["Sub-Category"] = "TBC"
-
-    # save merged df to csv
-    earliest_date = merged_df["Date"].min().strftime(FNAME_DATE) 
-    latest_date = merged_df["Date"].max().strftime(FNAME_DATE)
-    today_date = datetime.today().strftime(FNAME_DATE)
-    logger.info("Transactions found for this date range: %s to %s", earliest_date, latest_date)
-    fname = f"transactions_{earliest_date}_to_{latest_date}_imported_{today_date}"
-    fpath = os.path.join(CSV_DIR,fname + ".csv")
-    merged_df.to_csv(fpath)
-
-    # move iput csv to archive
-    # for f in input_files:
-    #     os.rename(f, os.path.join(ARCHIVE_DIR, os.path.basename(f) + ".csv"))
+    def merged_df_export(df):
+        # save merged df to csv
+        earliest_date = df["Date"].min().strftime(FNAME_DATE) 
+        latest_date = df["Date"].max().strftime(FNAME_DATE)
+        today_date = datetime.today().strftime(FNAME_DATE)
+        logger.info("Transactions found for this date range: %s to %s", earliest_date, latest_date)
+        fname = f"transactions_{earliest_date}_to_{latest_date}_imported_{today_date}"
+        fpath = os.path.join(CSV_DIR,fname + ".csv")
+        df.to_csv(fpath)
+        return fpath
     
-    return fpath
+    def archive_processed_csv():
+        # move iput csv to archive
+        pass
+        # for f in input_files:
+        #     os.rename(f, os.path.join(ARCHIVE_DIR, os.path.basename(f) + ".csv"))
+
+    df = get_input_files()
+    df = standardise_input_data(df)
+    df = add_columns(df)
+    df = format_tr_value(df)
+    df = trim_input_bloat(df)
+    merged_csv = merged_df_export(df)
+    print(merged_csv)
+
+
 
 def categorise_transactions(merged_csv):
     df = pd.read_csv(merged_csv)
