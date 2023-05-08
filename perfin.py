b86ea21a02c4b87ecd3ebe64607f05d7027f7506
purplespring-ae <alexander.evans@gmx.com>
2023-05-08 11:58:43 +0100

    Add Category and Sub-Category to import csv and prepare to categorise transactions. Started Account class which may or may not be needed

--- perfin.py
+++ perfin.py
@@ -12,6 +12,14 @@ import perflog
 
 # CLASS VARIABLES
 
+class Account():
+    def __init__(self, bank, label, csv_flag, is_credit) -> None:
+        self.bank = bank
+        self.label = label
+        self.csv_flag = csv_flag
+        self.is_credit = is_credit
+
+
 # GLOBAL VARIABLES
 INPUT_DIR = "./raw/new"
 ARCHIVE_DIR = "./raw/archive"
@@ -21,11 +29,12 @@ FNAME_DATE = "%Y-%m-%d"
 
 # INIT FUNCTIONS
 
-def grab_input():
+def import_new_csv():
     '''Look in INPUT_DIR for .csv files, create dataframe, move csv to archive'''
+
     input_files = (
         [os.path.join(INPUT_DIR, fname) for fname in os.listdir(INPUT_DIR)
-         if fname.endswith('.csv')]
+        if fname.endswith('.csv')]
         )
     if not input_files:
         logger.warning("No input files found.")
@@ -40,19 +49,34 @@ def grab_input():
         
     merged_df = pd.concat(dfs)
     merged_df = merged_df.reset_index(drop=True) # rectify duplicate indices
-    merged_df["Date"] = pd.to_datetime(merged_df["Date"], format="mixed") # CC statement 
+    merged_df["Date"] = pd.to_datetime(merged_df["Date"], format="mixed")
+    merged_df["Category"] = "Uncategorised"
+    merged_df["Sub-Category"] = "Uncategorised"
 
+    # save merged df to csv
     earliest_date = merged_df["Date"].min().strftime(FNAME_DATE) 
     latest_date = merged_df["Date"].max().strftime(FNAME_DATE)
     today_date = datetime.today().strftime(FNAME_DATE)
     logger.info("Transactions found for this date range: %s to %s", earliest_date, latest_date)
-    # save dataframe to csv - TODO: not needed?
     fname = f"transactions_{earliest_date}_to_{latest_date}_imported_{today_date}"
-    merged_df.to_csv(os.path.join(CSV_DIR,fname))
+    fpath = os.path.join(CSV_DIR,fname + ".csv")
+    merged_df.to_csv(fpath)
+
+    # move iput csv to archive merged
     for f in input_files:
-        # move iput csv to archive
-        os.rename(f, os.path.join(ARCHIVE_DIR, os.path.basename(f)))
+        os.rename(f, os.path.join(ARCHIVE_DIR, os.path.basename(f) + ".csv"))
+    
+    return fpath
 
-logger = perflog.logger(terminal_level=logging.DEBUG, file_level=0)
+def categorise_transactions(merged_csv):
+    pass
 
-grab_input()
+# MAIN ROUTINE
+
+if __name__ == "__main__":
+    # instantiate logger
+    logger = perflog.logger(terminal_level=logging.DEBUG, file_level=0)
+
+    # work with new data
+    fpath_new = import_new_csv()
+    categorise_transactions(fpath_new)
